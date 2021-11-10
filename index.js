80f9199ec49ee72d293f01a6b3f55fce104a5940
vishaljais00 <vishal.jais00@gmail.com>
2021-11-10 10:36:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function load(){
+    document.getElementById("loader").style.display="none";
+    
+}
+setTimeout(load , 7900);
+function load2(){
+    document.getElementById("example").style.display="none";
+    document.getElementById("sw").style.left="0vw";
+    document.getElementById("hp").style.display="none";
+}
+setTimeout(load2 , 19200);
+function playMusic(){
+    document.getElementById("music").play();
+}
